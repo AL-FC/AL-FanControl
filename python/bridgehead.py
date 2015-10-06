8ca4b6bb4a95096762f25829bffc545834d7b57b
Bengt Lüers <bengt.lueers@gmail.com>
2015-10-06 20:09:26 +0200

    remove the ruins

--- python/bridgehead.py
+++ python/bridgehead.py
@@ -1,27 +1,46 @@
-from time import sleep
+"""Python side of the Python to Arduino bridge."""
+
+from threading import Thread
 
 import serial
+from queue import Queue
 
-console = serial.Serial('/dev/ttyACM0', 9600)
 
-while True:
-    # console.write([42])
-
+def _reader(console, queue_read):
     while True:
         try:
-            while console.inWaiting() == 0:
-                sleep(0.1)
-                print('waiting for input')
-                message = '123;123;123'
-                console.writelines([message.encode('ASCII')])
+            response = console.readline().decode('ASCII')
+        except TypeError:
+            pass
+        response = response.strip()
+        if response != '':
+            queue_read.put(response)
 
-            response = ''
-            character = ''
-            while character != '\n':
-                character = console.read().decode('ASCII')
-                response += character
-                # print(character)
-            print(response)
-            break
-        except TypeError as e:
-            print(e)
+
+def _writer(console, queue_write):
+    while True:
+        message = queue_write.get()
+        console.writelines([message.encode('ASCII')])
+
+
+def main():
+    console = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    queue_read = Queue(maxsize=1)
+    queue_write = Queue(maxsize=1)
+
+    Thread(target=_reader, args=([console, queue_read])).start()
+    Thread(target=_writer, args=([console, queue_write])).start()
+
+    while True:
+        # TODO calculate conversation starter
+
+        message = '123;123;123'
+        queue_write.put(message)
+
+        pulse_durations = queue_read.get()
+        print(pulse_durations)
+
+        # TODO calculate response
+
+if __name__ == '__main__':
+    main()
