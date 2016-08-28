d6bed82bcd20cacfddf4aab7ae9d014d4823b92e
Bengt Lüers <bengt.lueers@gmail.com>
2016-08-29 00:37:59 +0200

    merge duplicate setup.py files

--- python/setup.py
+++ python/setup.py
@@ -1,10 +1,16 @@
+from __future__ import absolute_import, division, print_function
+
 from distutils.core import setup
 
 from setuptools import find_packages
 
 setup(name='fancontrol',
       version='0.1.0',
-      modules=['fancontrol'],
+      description='An Application-level Fan Control',
+      author='Bengt Lüers',
+      author_email='bengt.lueers@gmail.com',
+      url='https://github.com/Bengt/AL-FanControl',
       packages=find_packages(where="src"),
       package_dir={"": "src"},
-      )
+      py_modules=['foo'],
+      modules=['fancontrol'])
