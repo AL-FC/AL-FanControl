804ab99a3e74ed09abea936c17b2357ee30ac3dd
Bengt Lüers <bengt.lueers@gmail.com>
2016-09-28 18:09:43 +0200

    rewire version to use version file

--- python/setup.py
+++ python/setup.py
@@ -4,13 +4,15 @@ from distutils.core import setup
 
 from setuptools import find_packages
 
+version = open(
+    'src/fancontrol/version.py').read().split('= ')[1].rstrip('\n').strip('"')
+
 setup(name='fancontrol',
-      version='0.1.0',
+      version=version,
       description='An Application-level Fan Control',
       author='Bengt Lüers',
       author_email='bengt.lueers@gmail.com',
       url='https://github.com/Bengt/AL-FanControl',
       packages=find_packages(where="src"),
       package_dir={"": "src"},
-      py_modules=['foo'],
-      modules=['fancontrol'])
+      py_modules=['fancontrol'])
