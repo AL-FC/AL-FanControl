ec4313a51fc3531bd3ecdef2ccb3f05ed9afae84
Bengt Lüers <bengt.lueers@gmail.com>
2016-09-28 18:53:41 +0200

    less aggressive settings for my system

--- python/src/fancontrol/config/configuration.py
+++ python/src/fancontrol/config/configuration.py
@@ -19,6 +19,6 @@ class Configuration(object):
     ports = [0, 1, 2, 3, 4, 5, 6, 7]
     fans = [GPU, None, None, None, Case, Case, CPU, Case]
     chips = [radeon, None, None, None, it8718, it8718, k10temp, it8718]
-    features = [temp1, None, None, None, temp1, temp2, temp1, temp3]
+    features = [temp1, None, None, None, temp3, temp3, temp1, temp3]
     ambients = [25, None, None, None, 25, 25, 15, 25]
-    limits = [90, None, None, None, 80, 80, 70, 80]
+    limits = [90, None, None, None, 75, 75, 60, 75]
